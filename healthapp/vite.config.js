1d246f986cd4603bd3a17764b8014ee4a1f9a92b
pinkesh-gif <pinkesh@craywingz.com>
2026-05-18 20:02:45 +0530

    Update vite.config.js

--- healthapp/vite.config.js
+++ healthapp/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
+  base: '/veghealth-tracker/',
   plugins: [
     react(),
     VitePWA({
@@ -11,25 +12,16 @@ export default defineConfig({
       manifest: {
         name: 'VegHealth Tracker',
         short_name: 'VegHealth',
-        description: 'Personalized vegetarian nutrition & health tracker',
+        description: 'Personalized vegetarian nutrition tracker',
         theme_color: '#000000',
         background_color: '#000000',
         display: 'standalone',
         orientation: 'portrait',
-        scope: '/',
-        start_url: '/',
+        scope: '/veghealth-tracker/',
+        start_url: '/veghealth-tracker/',
         icons: [
           { src: 'icon-192.png', sizes: '192x192', type: 'image/png' },
           { src: 'icon-512.png', sizes: '512x512', type: 'image/png', purpose: 'any maskable' }
-        ]
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/generativelanguage\.googleapis\.com\/.*/i,
-            handler: 'NetworkOnly'
-          }
         ]
       }
     })
